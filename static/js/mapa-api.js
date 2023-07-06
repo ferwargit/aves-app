d7bb5a60371834dd0f068c7c2fbd6594b06ba4f8
Fernando Warno <ferwar@hotmail.com>
2023-07-06 17:58:57 -0300

    Opcion mapa-api observaciones del ave con marcador + info de la observacion + foto de la observacion

--- static/js/mapa-api.js
+++ static/js/mapa-api.js
@@ -1,6 +1,3 @@
-var overlays = {};
-
-
 var openStreetMap = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
@@ -23,71 +20,72 @@ var map = L.map('map', {
     layers: [openStreetMap]  // La primera capa base que se muestra por defecto
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-    const aveData = document.getElementById('aveData');
-    const nombreCientifico = aveData.getAttribute('data-nombre-cientifico');
-    console.log(nombreCientifico);
-    const nombreCientificoFormated = encodeURIComponent(nombreCientifico);
-    console.log(nombreCientificoFormated);
-
-fetch(`https://api.gbif.org/v1/species/match?name=${nombreCientificoFormated}&country=AR`)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-    // Aquí obtienes el usageKey de la respuesta de la API
-        var usageKey = data.usageKey;
-        console.log(usageKey);
-    // Continúa con el código para la segunda consulta a la API
-        fetch(`https://api.gbif.org/v2/map/occurrence/density/{z}/{x}/{y}@1x.png?taxonKey=${usageKey}&basisOfRecord=HUMAN_OBSERVATION&years=2023,2023&bin=hex&hexSize=64&style=red.poly&country=AR`)
-        .then(response => {
-            console.log(response);
-            if (response.ok) {
-            // Aquí obtienes la respuesta de la segunda consulta a la API
-                return response.blob();
-            } else {
-                throw new Error('Error en la solicitud');
-    }
-    })
-    .then(blob => {
-    // Continúa con el código para mostrar la respuesta en el mapa
-        var imageUrl = URL.createObjectURL(blob);
-
-        // Crea una nueva capa de mapa utilizando la imagen obtenida
-        var responseLayer = L.tileLayer(imageUrl, {
-        attribution: '&copy; <a href="https://www.gbif.org/">GBIF</a>',
-        opacity: 0.5
-        });
-    
-        // Agrega la capa de respuesta al mapa como una capa de superposición
-        overlays["Respuesta_de_API"] = responseLayer;
-        L.control.layers(baseLayers, overlays).addTo(map);
-
-
-
-    })
-    .catch(error => {
-    console.error('Error:', error);
-    });
-
-  })
-});
-
-
-// Aquí se definen las capas base (solo puede estar seleccionada una a la vez)
 var baseLayers = {
     "OpenStreetMap": openStreetMap,
     "Stamen Terrain": stamenTerrain,
     "Esri World Imagery": esriWorldImagery,
 };
 
-// Aquí se definen las capas de superposición (pueden estar seleccionadas varias a la vez)
-// var overlays = {
-//     "Caracara_plancus": caracara_plancus,
-// };
+// Agregar control de capas al mapa
+L.control.layers(baseLayers, {}).addTo(map);
 
-// Se añade el control de capas al mapa
-// L.control.layers(baseLayers, overlays).addTo(map);
+document.addEventListener('DOMContentLoaded', function() {
+    const aveData = document.getElementById('aveData');
+    const nombreCientifico = aveData.getAttribute('data-nombre-cientifico');
+    const nombreCientificoFormated = encodeURIComponent(nombreCientifico);
 
-// // Agregar escala al mapa
-// // L.control.scale().addTo(map);
-// L.control.scale({ position: 'bottomright' }).addTo(map);
+    fetch(`https://api.gbif.org/v1/species/match?name=${nombreCientificoFormated}&country=AR`)
+        .then(response => response.json())
+        .then(data => {
+            var usageKey = data.usageKey;
+            return fetch(`https://api.gbif.org/v1/occurrence/search?taxonKey=${usageKey}&country=AR&year=2023`);
+        })
+        .then(response => response.json())
+        .then(data => {
+            data.results.forEach(observation => {
+                if (observation.decimalLatitude && observation.decimalLongitude) {
+                    // Crea un nuevo marcador
+                    var marker = L.marker([observation.decimalLatitude, observation.decimalLongitude]).addTo(map);
+
+                    // Verifica si la propiedad extensions existe y tiene al menos un objeto dentro
+                    if (observation.extensions && observation.extensions["http://rs.gbif.org/terms/1.0/Multimedia"] && observation.extensions["http://rs.gbif.org/terms/1.0/Multimedia"].length > 0) {
+                        var multimedia = observation.extensions["http://rs.gbif.org/terms/1.0/Multimedia"][0];
+                        var photoLink = multimedia["http://purl.org/dc/terms/identifier"];
+                        var place = observation.stateProvince;
+
+                        var eventDate = observation.eventDate;
+                        let eventDateObj = new Date(eventDate);
+                        let formattedDate = `${eventDateObj.getDate()}-${eventDateObj.getMonth() + 1}-${eventDateObj.getFullYear()}`;
+
+                        var eventTime = observation.eventTime;
+                        var timeWithoutZone = eventTime.split('-')[0]; // Ignorar la zona horaria
+                        var formattedTime = moment(timeWithoutZone, "HH:mm:ss").format('HH:mm');
+
+
+                        var latitude = observation.decimalLatitude;
+                        var longitude = observation.decimalLongitude;
+                        var photoAuthor = multimedia["http://purl.org/dc/terms/creator"];
+
+                        var popupContent = `
+                        <p><a href="${photoLink}" target="_blank">Ver foto</a></p>
+                        <p><strong>Lugar:</strong> ${place}</p>
+                        
+                        <p><strong>Fecha:</strong> ${formattedDate}</p>
+                        
+                        <p><strong>Hora:</strong> ${formattedTime}</p>
+                        <p><strong>Latitud:</strong> ${latitude}</p>
+                        <p><strong>Longitud:</strong> ${longitude}</p>
+                        <p><strong>Autor:</strong> ${photoAuthor}</p>
+                        `;
+
+
+                        marker.bindPopup(popupContent);
+                    }
+
+                }
+            });
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+});
