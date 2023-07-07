9ee717ca1cc67c77cd6e477bf8f27d8b94fa2bb1
Fernando Warno <ferwar@hotmail.com>
2023-07-07 07:50:03 -0300

    Se elimina la urls de logeado en urls.py

--- user/urls.py
+++ user/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
     path('create_user/', views.create_user, name='create_user'),
     path('login_user/', views.login_user, name='login'),
     path('logout_user/', views.logout_user, name='logout'),
-    path('logeado/', views.logeado, name='logeado'),
     
     path('create_list/', CrearAvistaje.as_view(), name='crear_avistaje'),
     path('list_avistaje/', ListarAvistajes.as_view(), name='listar_avistajes'),
