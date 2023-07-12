0df63e6aca3e7db71a110d96af552d28941f888f
Fernando Warno <ferwar@hotmail.com>
2023-07-12 06:38:55 -0300

    1.-Ajuste diseño de titulo Appeves en home.html. Se ajusta css. 2.-Se ajusta fotter.html y footer.css. Se pasan estilos a css. 3.-Se ajusta base.html y css. Se posiciona la barra de navegacion en la parte superior de manera fija mientras el usuario se desplaza verticalmente. 4.-Se re-diseña login_user.html y login_user.css a Responsivo. 5.-Se rediseña toda la estructura del formulario renderizando los campos del formulario de manera individual. Se ajusta diseño en css a Responsivo. 6.-En views.py se elimina importacion en desuso y se da formateo al codigo..

--- user/views.py
+++ user/views.py
@@ -1,7 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView
@@ -11,51 +11,54 @@ from .models import Avistaje
 
 
 def quienes(request):
-    return render(request, 'user/quienes.html')
+    return render(request, "user/quienes.html")
+
 
 def create_user(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Te has registrado con éxito')
-            return redirect('home')
+            messages.success(request, "Te has registrado con éxito")
+            return redirect("home")
     else:
         form = CustomUserCreationForm()
-    return render(request, 'user/create_user.html', {'form': form})
+    return render(request, "user/create_user.html", {"form": form})
+
 
 def login_user(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = UserLoginForm(request.POST)
         if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
             user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user=user)
-                return redirect('home')
+                return redirect("home")
             else:
-                messages.error(request, 'Usuario o contraseña incorrectos')
+                messages.error(request, "Usuario o contraseña incorrectos")
     else:
         form = UserLoginForm()
-    return render(request, 'user/login_user.html', {'form': form})
+    return render(request, "user/login_user.html", {"form": form})
 
 
 def logout_user(request):
     logout(request)
-    return redirect('home')
+    return redirect("home")
 
 
 class CrearAvistaje(LoginRequiredMixin, CreateView):
     model = Avistaje
     form_class = AvistajeForm
-    template_name = 'user/crear_lista.html'
-    success_url = reverse_lazy('listar_avistajes')
+    template_name = "user/crear_lista.html"
+    success_url = reverse_lazy("listar_avistajes")
+
 
 class ListarAvistajes(LoginRequiredMixin, ListView):
     model = Avistaje
-    template_name = 'user/lista_avistajes.html'
-    context_object_name = 'avistajes'
+    template_name = "user/lista_avistajes.html"
+    context_object_name = "avistajes"
 
     def get_queryset(self):
         self.queryset = Avistaje.objects.filter(id_user_id=self.request.user.pk)
