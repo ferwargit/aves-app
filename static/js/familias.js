2bd7824a39b6d6fa575731bedbb814ec11d0286c
Fernando Warno <ferwar@hotmail.com>
2023-07-24 18:46:38 -0300

    1.-Se crea mapa con la visualizacion de las observaciones por familia y por especie (color). Se codifica familias.html, css y js.

--- static/js/familias.js
+++ static/js/familias.js
@@ -1,36 +1,177 @@
+document.addEventListener("DOMContentLoaded", function () {
+  inicializarMapa();
+  // Realiza otras inicializaciones aquí
+});
+
+// Variables globales
+let map;
+let markersLayer;
+// Función para mostrar el detalle de la familia seleccionada.
 function mostrarDetalle() {
   var select = document.getElementById("familia");
   var detalle = document.getElementById("detalle");
   var detalleNombre = document.getElementById("detalle-nombre");
-  // var detalleNombreCientifico = document.getElementById("detalle-nombre-cientifico");
 
   if (select.value !== "") {
     detalle.style.display = "block";
     detalleNombre.textContent = select.value;
-    obtenerDetalle(select.value); // Llamada a la función para obtener el nombre y el nombre científico
-  } else {
-    detalle.style.display = "none";
+    obtenerDetalle(select.value);
+    // Llamada a la función para obtener el usageKey utilizando async/await
+    obtenerUsageKey(select.value)
+      .then((taxonKey) => {
+        return obtenerObservaciones(taxonKey);
+      })
+      .then((data) => {
+        // Actualizar los marcadores en el mapa con los nuevos datos
+        actualizarMarcadores(data);
+      })
+      .catch((error) => {
+        console.error("Error al obtener datos:", error);
+      });
   }
 }
 
+// Función para inicializar el mapa.
+function inicializarMapa() {
+  map = L.map("map").setView([-41.6118, -62.4173], 5); // Coordenadas del centro de Argentina y nivel de zoom
+
+  // Agregar una capa de mapa base (puedes usar otras capas disponibles)
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "&copy; OpenStreetMap contributors",
+  }).addTo(map);
+
+  markersLayer = L.layerGroup().addTo(map); // Capa para los marcadores
+}
+
+// Función para actualizar los marcadores en el mapa con nuevos datos.
+function actualizarMarcadores(data) {
+  // Comprobar si markersLayer está definido antes de llamar a clearLayers
+  if (markersLayer) {
+    markersLayer.clearLayers(); // Limpiar los marcadores existentes
+  } else {
+    console.error("markersLayer no está definido");
+  }
+
+  // Diccionario para mapear el nombre de la especie con un color específico
+  const speciesColor = {
+    "Chloephaga picta": "red",
+    "Anas flavirostris": "blue",
+    "Coscoroba coscoroba": "yellow",
+    "Netta peposaca": "orange",
+    "Oxyura vittata": "purple",
+    // Agrega más especies con sus colores aquí
+  };
+
+  // Iterar sobre los datos de las especies
+  data.results.forEach((species) => {
+    const lat = species.decimalLatitude;
+    const lng = species.decimalLongitude;
+    const name = species.species;
+    const color = speciesColor[name] || "green"; // Si la especie no tiene color asignado, usar azul por defecto
+
+    // Crear un marcador y agregarlo a la capa de marcadores
+    const marker = L.marker([lat, lng], {
+      icon: L.divIcon({
+        className: "my-marker",
+        iconSize: [10, 10],
+        html: `<div style="background-color: ${color};"></div>`,
+      }),
+    });
+
+    markersLayer.addLayer(marker); // Agregar el marcador a la capa de marcadores
+    // map.addLayer(markersLayer); // Asegúrate de que la capa de marcadores se agrega al mapa
+  });
+}
+
+// Función para obtener el detalle de la familia.
 function obtenerDetalle(familia) {
-  fetch(`/obtener_detalle_familia?nombre_familia=${familia}`)
+  const url = `/obtener_detalle_familia?nombre_familia=${familia}`;
+  fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      
-      // Establecer el contenido del nombre y nombre científico en los elementos correspondientes
       var detalleNombre = document.getElementById("detalle-nombre");
       var detalleNombreCientifico = document.getElementById(
         "detalle-nombre-cientifico"
       );
       detalleNombre.textContent = data.nombre;
       detalleNombreCientifico.textContent = data.nombre_cientifico;
-      // Mostrar la información en la consola
-      console.log(data);
-      console.log("Nombre: ", data.nombre);
-      console.log("Nombre Científico: ", data.nombre_cientifico);
+      console.log("Detalle:", data);
     })
     .catch((error) => {
-      console.error("Error:", error);
+      console.error("Error al obtener detalle:", error);
+    });
+}
+
+// Función para obtener el usageKey de la familia desde la API utilizando async/await.
+async function obtenerUsageKey(familia) {
+  const url = `https://api.gbif.org/v1/species/match?rank=family&name=${encodeURIComponent(
+    familia
+  )}`;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.json();
+    const taxonKey = data.usageKey;
+    console.log("TaxonKey de", familia, ":", taxonKey);
+    return taxonKey; // Devolver el taxonKey para que esté disponible en el llamado a obtenerUsageKey
+  } catch (error) {
+    console.error("Error al obtener taxonKey:", error);
+    throw error; // Lanzar el error para manejarlo en el llamado a obtenerUsageKey
+  }
+}
+
+// Función para obtener las observaciones de especies utilizando el taxonKey.
+function obtenerObservaciones(taxonKey) {
+  const url = `https://api.gbif.org/v1/occurrence/search?rank=species&taxonKey=${taxonKey}&country=AR&year=2023&limit=200`;
+
+  return fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data); // Verificar los datos recibidos de la API
+
+      // Diccionario para mapear el nombre de la especie con un color específico
+      const speciesColor = {
+        "Chloephaga picta": "red",
+        "Anas flavirostris": "blue",
+        "Coscoroba coscoroba": "yellow",
+        "Netta peposaca": "orange",
+        "Oxyura vittata": "purple",
+        // Agrega más especies con sus colores aquí
+      };
+
+      // Iterar sobre los datos de las especies
+      data.results.forEach((species) => {
+        const lat = species.decimalLatitude;
+        console.log("lat: ", lat);
+        const lng = species.decimalLongitude;
+        console.log("lng: ", lng);
+        const name = species.species;
+        console.log("name: ", name);
+        const color = speciesColor[name] || "green"; // Si la especie no tiene color asignado, usar azul por defecto
+
+        // Crear un marcador y agregarlo al mapa
+        const marker = L.marker([lat, lng], {
+          icon: L.divIcon({
+            className: "my-marker",
+            iconSize: [10, 10],
+            html: `<div style="background-color: ${color};"></div>`,
+          }),
+          // }).addTo(map);
+        });
+        markersLayer.addLayer(marker); // Agregar el marcador a la capa de marcadores
+      });
+
+      return data; // Devolvemos los datos obtenidos de la API
+    })
+    .catch((error) => {
+      console.error("Error al obtener observaciones:", error);
+      throw error; // Lanzamos el error para que pueda ser capturado en la función mostrarDetalle si es necesario
     });
 }
