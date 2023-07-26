17dafca329bce46e7a75eb0fe00aae0c7485e404
Fernando Warno <ferwar@hotmail.com>
2023-07-26 06:45:15 -0300

    1.-En familias.json se completa con texto el campo descripcion para trabajar el disenio. 2.-Se termina de re-codificar el familias.js para que funcione con el nuevo formato de familias.json y muestre tambien las especies de familias segun los datos de las observaciones de la consulta a la API. 3.-Se ajusta familias.html y familias.css.

--- static/js/familias.js
+++ static/js/familias.js
@@ -1,86 +1,30 @@
-document.addEventListener("DOMContentLoaded", function () {
-  inicializarMapa();
-  // Realiza otras inicializaciones aquí
-});
-
-// Variables globales
-let map;
-let markersLayer;
+let especiesDiv;
 // Función para mostrar el detalle de la familia seleccionada.
 function mostrarDetalle() {
-  var select = document.getElementById("familia");
-  var detalle = document.getElementById("detalle");
-  var detalleNombre = document.getElementById("detalle-nombre");
+  // Obtener el valor seleccionado en el elemento select
+  const select = document.getElementById("familia");
+  const detalle = document.getElementById("detalle");
+  const detalleNombre = document.getElementById("detalle-nombre");
 
   if (select.value !== "") {
     detalle.style.display = "block";
     detalleNombre.textContent = select.value;
     obtenerDetalle(select.value);
+
     // Llamada a la función para obtener el usageKey utilizando async/await
     obtenerUsageKey(select.value)
-      .then((taxonKey) => {
-        return obtenerNombresYConteo(taxonKey);
-      })
+      .then(obtenerNombresYConteo)
       .then((data) => {
-        // Actualizar los marcadores en el mapa con los nuevos datos
-        actualizarMarcadores(data);
+        // Actualizar los marcadores en el mapa con los nuevos datos y pasar el div de especies
+        actualizarMarcadores(data, especiesDiv);
       })
       .catch((error) => {
         console.error("Error al obtener datos:", error);
       });
-  }
-}
-
-// Función para inicializar el mapa.
-function inicializarMapa() {
-  map = L.map("map").setView([-41.6118, -62.4173], 5); // Coordenadas del centro de Argentina y nivel de zoom
-
-  // Agregar una capa de mapa base (puedes usar otras capas disponibles)
-  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution: "&copy; OpenStreetMap contributors",
-  }).addTo(map);
-
-  markersLayer = L.layerGroup().addTo(map); // Capa para los marcadores
-}
-
-// Función para actualizar los marcadores en el mapa con nuevos datos.
-function actualizarMarcadores(data) {
-  // Comprobar si markersLayer está definido antes de llamar a clearLayers
-  if (markersLayer) {
-    markersLayer.clearLayers(); // Limpiar los marcadores existentes
   } else {
-    console.error("markersLayer no está definido");
+    // Ocultar el detalle cuando no hay información seleccionada
+    detalle.style.display = "none";
   }
-
-  // Diccionario para mapear el nombre de la especie con un color específico
-  const speciesColor = {
-    "Cathartes aura": "red",
-    "Cathartes burrovianus": "blue",
-    "Coragyps atratus": "yellow",
-    "Sarcoramphus papa": "orange",
-    "Vultur gryphus": "purple",
-    // Agrega más especies con sus colores aquí
-  };
-
-  // Iterar sobre los datos de las especies
-  data.results.forEach((species) => {
-    const lat = species.decimalLatitude;
-    const lng = species.decimalLongitude;
-    const name = species.species;
-    const color = speciesColor[name] || "green"; // Si la especie no tiene color asignado, usar azul por defecto
-
-    // Crear un marcador y agregarlo a la capa de marcadores
-    const marker = L.marker([lat, lng], {
-      icon: L.divIcon({
-        className: "my-marker",
-        iconSize: [10, 10],
-        html: `<div style="background-color: ${color};"></div>`,
-      }),
-    });
-
-    markersLayer.addLayer(marker); // Agregar el marcador a la capa de marcadores
-    // map.addLayer(markersLayer); // Asegúrate de que la capa de marcadores se agrega al mapa
-  });
 }
 
 // Función para obtener el detalle de la familia.
@@ -130,77 +74,12 @@ async function obtenerUsageKey(familia) {
   }
 }
 
-// Función para obtener las observaciones de especies utilizando el taxonKey.
-function obtenerObservaciones(taxonKey) {
-  const url = `https://api.gbif.org/v1/occurrence/search?rank=species&taxonKey=${taxonKey}&country=AR&year=2023&limit=300`;
-
-  return fetch(url)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      console.log(data); // Verificar los datos recibidos de la API
-
-      // Diccionario para mapear el nombre de la especie con un color específico
-      const speciesColor = {
-        "Cathartes aura": "red",
-        "Cathartes burrovianus": "blue",
-        "Coragyps atratus": "yellow",
-        "Sarcoramphus papa": "orange",
-        "Vultur gryphus": "purple",
-        // Agrega más especies con sus colores aquí
-      };
-
-      // Iterar sobre los datos de las especies
-      data.results.forEach((species) => {
-        const lat = species.decimalLatitude;
-        console.log("lat: ", lat);
-        const lng = species.decimalLongitude;
-        console.log("lng: ", lng);
-        const name = species.species;
-        console.log("name: ", name);
-        const color = speciesColor[name] || "green"; // Si la especie no tiene color asignado, usa green por defecto
-
-        // Crear un marcador y agregarlo al mapa
-        const marker = L.marker([lat, lng], {
-          icon: L.divIcon({
-            className: "my-marker",
-            iconSize: [10, 10],
-            html: `<div style="background-color: ${color};"></div>`,
-          }),
-          // }).addTo(map);
-        });
-        markersLayer.addLayer(marker); // Agregar el marcador a la capa de marcadores
-      });
-
-      return data; // Devolvemos los datos obtenidos de la API
-    })
-    .catch((error) => {
-      console.error("Error al obtener observaciones:", error);
-      throw error; // Lanzamos el error para que pueda ser capturado en la función mostrarDetalle si es necesario
-    });
-}
-
 function obtenerNombresYConteo(taxonKey) {
   return obtenerObservaciones(taxonKey)
     .then((data) => {
       const { results } = data;
       const speciesCount = {};
       const uniqueSpeciesNames = [];
-
-      // Diccionario para mapear el nombre de la especie con un color específico
-      const speciesColor = {
-        "Cathartes aura": "red",
-        "Cathartes burrovianus": "blue",
-        "Coragyps atratus": "yellow",
-        "Sarcoramphus papa": "orange",
-        "Vultur gryphus": "purple",
-        // Agrega más especies con sus colores aquí
-        // Agrega más especies con sus colores aquí
-      };
 
       results.forEach((species) => {
         const name = species.species;
@@ -222,4 +101,128 @@ function obtenerNombresYConteo(taxonKey) {
       console.error("Error al obtener nombres y conteo:", error);
       throw error;
     });
+}
+
+// Función para obtener las observaciones de especies utilizando el taxonKey.
+function obtenerObservaciones(taxonKey) {
+  const url = `https://api.gbif.org/v1/occurrence/search?rank=species&taxonKey=${taxonKey}&country=AR&year=2023&limit=300`;
+
+  return fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data); // Verificar los datos recibidos de la API
+
+      // Diccionario para mapear el nombre de la especie con un color específico
+      const speciesColor = {
+        // Agrega más especies con sus colores aquí
+      };
+
+      // Iterar sobre los datos de las especies
+      data.results.forEach((species) => {
+        const lat = species.decimalLatitude;
+        console.log("lat: ", lat);
+        const lng = species.decimalLongitude;
+        console.log("lng: ", lng);
+        const name = species.species;
+        console.log("name: ", name);
+        const color = speciesColor[name] || "green"; // Si la especie no tiene color asignado, usa verde por defecto
+
+        // Crear un marcador y agregarlo al mapa
+        const marker = L.marker([lat, lng], {
+          icon: L.divIcon({
+            className: "my-marker",
+            iconSize: [10, 10],
+            html: `<div style="background-color: ${color};"></div>`,
+          }),
+        });
+        markersLayer.addLayer(marker); // Agregar el marcador a la capa de marcadores
+      });
+
+      return data; // Devolvemos los datos obtenidos de la API
+    })
+    .catch((error) => {
+      console.error("Error al obtener observaciones:", error);
+      throw error; // Lanzamos el error para que pueda ser capturado en la función mostrarDetalle si es necesario
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Acceder al div de especies y almacenarlo en una variable
+  especiesDiv = document.getElementById("especies");
+
+  // Inicializar el mapa al cargar la página
+  inicializarMapa();
+
+  // Evento para mostrar el detalle de la familia seleccionada
+  document.getElementById("familia").addEventListener("change", mostrarDetalle);
+
+  // Función para inicializar el mapa.
+  function inicializarMapa() {
+    map = L.map("map").setView([-41.6118, -62.4173], 5); // Coordenadas del centro de Argentina y nivel de zoom
+
+    // Agregar una capa de mapa base de OpenStreetMap
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution: "&copy; OpenStreetMap contributors",
+    }).addTo(map);
+
+    markersLayer = L.layerGroup().addTo(map); // Capa para los marcadores
+  }
+});
+
+// Función para actualizar los marcadores en el mapa con nuevos datos.
+function actualizarMarcadores(data) {
+  // Comprobar si markersLayer está definido antes de llamar a clearLayers
+  if (markersLayer) {
+    markersLayer.clearLayers(); // Limpiar los marcadores existentes
+  } else {
+    console.error("markersLayer no está definido");
+  }
+
+  // Iterar sobre los datos de las especies
+  data.results.forEach((species) => {
+    const lat = species.decimalLatitude;
+    const lng = species.decimalLongitude;
+
+    // Establecer el color verde por defecto
+    const color = "green";
+
+    // Crear un marcador y agregarlo a la capa de marcadores
+    const marker = L.marker([lat, lng], {
+      icon: L.divIcon({
+        className: "my-marker",
+        iconSize: [10, 10],
+        html: `<div style="background-color: ${color};"></div>`,
+      }),
+    });
+
+    markersLayer.addLayer(marker); // Agregar el marcador a la capa de marcadores
+
+    // Ordenar el array de nombres de especies alfabéticamente
+    data.uniqueSpeciesNames.sort();
+
+    // Limpiar el contenido previo del div de especies
+    especiesDiv.innerHTML = "";
+
+    // Agregar el título
+    const titulo = document.createElement("h3");
+    titulo.textContent = "Especies de Aves Avistadas";
+    especiesDiv.appendChild(titulo);
+
+    // Mostrar los datos
+    const especiesList = document.createElement("ul");
+    data.uniqueSpeciesNames.forEach((species) => {
+      const li = document.createElement("li");
+      li.textContent = species;
+      especiesList.appendChild(li);
+    });
+
+    especiesDiv.appendChild(especiesList); // Agregamos la lista de especies al div
+  });
+  // Mostrar el contenedor de especies cuando haya información disponible
+  especiesDiv.style.display = "block";
 }
