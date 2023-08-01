8a729f9ab0c1af01f522c43769c2bba94375d70a
Fernando Warno <ferwar@hotmail.com>
2023-07-31 21:06:18 -0300

    1.-Ajuste SECRET_KEY default en settings.py

--- core/settings.py
+++ core/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-)fr03!0%wlt1z--wx2m+0c!5ks2npvps-r%@$c!(d5#_n--out'
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
+SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-)fr03!0%wlt1z--wx2m+0c!5ks2npvps-r%@$c!(d5#_n--out')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
