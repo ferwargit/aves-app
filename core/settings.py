cd49c628365ead45014c58675265ad4aa3e2c17b
Fernando Warno <ferwar@hotmail.com>
2023-08-01 18:11:50 -0300

    1.-Quite el STATIC_ROOT fuera del if paara deployar en Render.

--- core/settings.py
+++ core/settings.py
@@ -143,12 +143,12 @@ STATIC_URL = '/static/'
 #STATICFILES_DIRS = [
 #    BASE_DIR / "static",
 
+# Tell Django to copy statics to the `staticfiles` directory
+# in your application directory on Render.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Following settings only make sense on production and may break development environments.
 if 'RENDER' not in os.environ:
-    # Tell Django to copy statics to the `staticfiles` directory
-    # in your application directory on Render.
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
