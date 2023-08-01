e64ac60ad9713e1c5e52b8a893fb999df6fbe562
Fernando Warno <ferwar@hotmail.com>
2023-07-31 22:38:52 -0300

    Cambios loaddata

--- build.sh
+++ build.sh
@@ -6,3 +6,5 @@ set -o errexit
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+python manage.py loaddata province.json status.json familias.json grupos.json habitat.json movimientos_estacionales.json plumajes.json tamanios_y_formas.json estados.json birds.json
